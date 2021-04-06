1ea48e3cd663cb185e4c45f479d84b196fff9e8c
chernyshevdv <chernyshevdv@gmail.com>
2021-04-06 17:59:43 +0300

    Create templates, finalize API, start developing frontend

--- api/deployment/deployment.py
+++ api/deployment/deployment.py
@@ -1,7 +1,9 @@
 import os
 import sqlite3
 from types import resolve_bases
-from flask import Flask, json, jsonify, request, redirect, g
+from flask import Flask, jsonify, request, redirect, g
+from requests.api import get
+import json
 
 deployment = Flask(__name__)
 
@@ -17,25 +19,95 @@ def get_db():
 
 @deployment.route('/deployment/<id>', methods=['GET'])
 def read(id):
+    """ Retrieve a deployment by ID
+    ---
+    get:
+        summary: retrieve a deployment by ID
+        parameters:
+            - name: id
+              in: path
+              description: deployment ID
+              type: integer
+              required: true
+        responses:
+            200:
+                description: OK. record found
+                schenma:
+                    success: true
+                    deployment:
+                        id: 4
+                        title: "my deployment"
+                        excel_columns: "A,B,C"
+                        excel_header_row: 0
+            404:
+                description: Error. Deployment with this ID is not found
+                schema:
+                    - success: False
+                    - error: Deployment 1 is not found
+    """
     m_sql = "SELECT * FROM deployment WHERE id=:id"
     m_rs = get_db().execute(m_sql, {"id": id}).fetchone()
     if m_rs is None:
         return jsonify(success=False, error=f"Deployment {id} is not found"), 404
     else:
-        m_dict = dict(m_rs)
-        m_dict['success'] = True
-        return jsonify(m_dict)
+        m_ret = {'success': True}
+        m_ret['deployment'] = dict(m_rs)
+        return jsonify(m_ret)
 
 @deployment.route('/deployment', methods=['PUT'])
 def create():
+    """ Create a new deployment
+    ---
+    put:
+        parameters:
+            json:
+                - name: title
+                  type: string
+                  required: true
+                - name: excel_columns
+                  description: a list of columns to use in import.
+                  type: string
+                  required: false
+                - name: excel_header_row
+                  description: number of excel row where data header is located
+                  type: string
+                  required: false
+        responses:
+            200:
+                description: OK. record found
+                schenma:
+                    success: true
+                    deployment:
+                        id: 4
+                        title: "my deployment"
+                        excel_columns: "A,B,C"
+                        excel_header_row: 0
+            501:
+                description: Error. No JSON found in the request
+                schema:
+                    success: False
+                    error: No JSON found in the request
+            503:
+                description: Integrity error. Record with this title exists already.
+                schema:
+                    success: False
+                    error: Deployment my title exists already
+            504:
+                description: Error - a record has not been created.
+                schema:
+                    success: False
+                    error: Deployment my title has not been created
+    """
     if not request.json:
-        return jsonify(success=False, error="No JSON found in the request"), 51
+        return jsonify(success=False, error="No JSON found in the request"), 501
 
     m_sql = "INSERT INTO deployment (title, excel_columns, excel_header_row) VALUES (:title, :excel_columns, :excel_header_row)"
+    l_json = json.loads(request.json)
+    print(f"JSON got: {request.json}")
     m_deployment_data = {
-        'title': request.json.get('title', ''),
-        'excel_columns': request.json.get('excel_columns', ''),
-        'excel_header_row': request.json.get('excel_header_row', '0')
+        'title': l_json['title'],
+        'excel_columns': l_json['excel_columns'],
+        'excel_header_row': l_json['excel_header_row']
     }
     conn = get_db()
     try:
@@ -45,28 +117,72 @@ def create():
         return jsonify(success=False, error=f"Deployment {m_deployment_data['title']} exists already"), 503
     m_rs = conn.execute("SELECT id FROM deployment WHERE title=:title", {"title": m_deployment_data['title']}).fetchone()
     if m_rs is None:
-        return jsonify(success=False, error=f"Deployment {title} has not been created")
+        return jsonify(success=False, error=f"Deployment {m_deployment_data['title']} has not been created"), 504
     else:
         m_id = m_rs[0]
         return redirect(f"/deployment/{m_id}")
 
 @deployment.route('/deployment', methods=['POST'])
 def update():
+    """ Update an existing deployment
+    ---
+    post:
+        parameters:
+            json:
+                - name: id
+                  type: integer
+                  required: true
+                - name: title
+                  type: string
+                  required: false
+                - name: excel_columns
+                  description: a list of columns to use in import.
+                  type: string
+                  required: false
+                - name: excel_header_row
+                  description: number of excel row where data header is located
+                  type: string
+                  required: false
+        responses:
+            200:
+                description: OK. record updated
+                schenma:
+                    - id (just a number)
+            404:
+                description: Error - a record to update has not been found.
+                schema:
+                    - success: False
+                    - error: 0 records has been updated instead of 1.
+            501:
+                description: Error. No JSON found in the request
+                schema:
+                    - success: False
+                    - error: No JSON found in the request
+            502:
+                description: Error. JSON format is incorrect
+                schema:
+                    - success: False
+                    - error: JSON format is incorrect: no id and/or title
+    """
     if not request.json:
         return jsonify(success=False, error="No JSON found in the request"), 501
-    m_id = request.json.get('id')
-    m_title = request.json.get('title')
-    if m_title is None or m_id is None:
+    
+    l_json = json.loads(request.json)
+    print(f"Got the following JSON: {l_json}")
+    m_id = l_json['id']
+    if m_id is None:
         return jsonify(success=False, error="JSON format is incorrect: no id and/or title"), 502
     
-    UPDATE_PREFIX = "UPDATE deployment SET title=:title"
-    m_deployment_data = {'title': m_title}
+    UPDATE_PREFIX = "UPDATE deployment SET "
     
     COLUMNS_BODY = ""
-    for m_column in ['excel_columns', 'excel_header_row']:
-        if request.json.get(m_column):
-            COLUMNS_BODY += f", {m_column}=:{m_column}"
-            m_deployment_data[m_column] = request.json.get(m_column)
+    m_deployment_data = {}
+    for m_column in ['title', 'excel_columns', 'excel_header_row']:
+        if l_json[m_column]:
+            if len(m_deployment_data) > 0:
+                COLUMNS_BODY += ", "
+            COLUMNS_BODY += f"{m_column}=:{m_column}"
+            m_deployment_data[m_column] = l_json[m_column]
 
     WHERE_SUFFIX = " WHERE id=:id"
     m_deployment_data['id'] = m_id
@@ -86,19 +202,13 @@ def update():
     except sqlite3.Error as m_error:
         return jsonify(success=False, error=m_error)
 
-@deployment.route('/deployment', methods=['DELETE'])
-def delete():
-    if not request.json:
-        return jsonify(success=False, error="No JSON found in the request"), 501
-    m_id = request.json.get('id')
-    if m_id is None:
-        return jsonify(success=False, error="JSON format is incorrect: no id and/or title"), 502
-    
+@deployment.route('/deployment/<id>', methods=['DELETE'])
+def delete(id):
     m_sql = "DELETE FROM deployment WHERE id=:id"
     try:
         m_conn = get_db()
         m_cursor = m_conn.cursor()
-        m_cursor.execute(m_sql, {'id': m_id})
+        m_cursor.execute(m_sql, {'id': id})
         m_conn.commit()
         m_rows_updated = m_cursor.rowcount
         if m_rows_updated != 1:
@@ -107,6 +217,27 @@ def delete():
             return jsonify(success=True, id=m_id)
     except sqlite3.Error as m_error:
         return jsonify(success=False, error=m_error)
+
+@deployment.route('/deployment/list', methods=['GET'])
+def list():
+    m_sql = "SELECT id, title, excel_columns, excel_header_row FROM deployment"
+    try:
+        m_conn = get_db()
+        m_cursor = m_conn.cursor()
+        m_rs = m_cursor.execute(m_sql).fetchall()
+        m_data = []
+        for m_row in m_rs:
+            m_data.append({
+                'id': m_row['id'],
+                'title': m_row['title'],
+                'excel_columns': m_row['excel_columns'],
+                'excel_header_row': m_row['excel_header_row']
+                })
+        m_ret = {'success': True}
+        m_ret['deployments'] = m_data
+        return json.dumps(m_ret)
+    except sqlite3.Error as m_error:
+        return json.dumps({'success': False, 'error': m_error})
 
 
 if __name__ == "__main__":
--- web/frontend.py
+++ web/frontend.py
@@ -1,5 +1,16 @@
 import os
-from flask import Flask, request
+from flask.helpers import make_response
+import requests
+from flask import Flask, request, render_template, jsonify, json, redirect
+from werkzeug.datastructures import Headers
+
+JSON_HEADERS = {"Content-type": "application/json"}
+DEPLOYMENT_HOST = os.environ.get("DEPLOYMENT_HOST", "localhost")
+DEPLOYMENT_PORT = os.environ.get("DEPLOYMENT_PORT", "5000")
+DEPLOYMENT_URL_BASE = f"http://{DEPLOYMENT_HOST}:{DEPLOYMENT_PORT}"
+DEPLOYMENT_URL_CREATE = f"{DEPLOYMENT_URL_BASE}/deployment"
+DEPLOYMENT_URL_UPDATE = DEPLOYMENT_URL_CREATE
+DEPLOYMENT_URL_LIST = f"{DEPLOYMENT_URL_BASE}/deployment/list"
 
 frontend = Flask(__name__)
 
@@ -8,29 +19,63 @@ def home():
     return 'Hello world'
 
 @frontend.route("/deployment/new", methods=['GET'])
-def show_new_deployment_form(id):
-    pass
+def show_new_deployment_form():
+    return render_template('new_deployment.j2')
 
-@frontend.route("/deployment", methods=['PUT'])
+@frontend.route("/deployment/new", methods=['POST'])
 def deployment_create():
-    pass
+    l_data = {}
+    for key in ['title', 'excel_columns', 'excel_header_row']:
+        l_data[key] = request.form[key]
+    l_data_json = json.dumps(l_data)
+    print(f"JSON data: {l_data_json}")
+    l_creation_response = requests.put(DEPLOYMENT_URL_CREATE, json=l_data_json, headers=JSON_HEADERS)
+    l_resp_json = l_creation_response.json()
+    l_id = l_resp_json['id']
+
+    return redirect(f"/deployment/{l_id}")
+
 
 @frontend.route("/deployment/<id>", methods=['GET'])
 def show_deployment_view(id):
-    pass
+    l_url = f"{DEPLOYMENT_URL_BASE}/deployment/{id}"
+    l_api_response = requests.get(l_url)
+    
+    return render_template('view_deployment.j2', obj=l_api_response.json())
 
 @frontend.route("/deployment/edit/<id>", methods=['GET'])
 def show_deployment_form(id):
-    pass
+    l_url = f"{DEPLOYMENT_URL_BASE}/deployment/{id}"
+    l_api_response = requests.get(l_url)
+
+    return render_template('edit_deployment.j2', obj=l_api_response.json())
 
 @frontend.route("/deployment/update", methods=['POST'])
 def update_deployment():
-    pass
+    l_data = {}
+    for key in ['id', 'title', 'excel_columns', 'excel_header_row']:
+        l_data[key] = request.form[key]
+    l_data_json = json.dumps(l_data)
+    print(f"Going to send the following JSON: {l_data_json}")
+    l_update_response = requests.post(DEPLOYMENT_URL_UPDATE, json=l_data_json, headers=JSON_HEADERS)
+    l_resp_json = l_update_response.json()
+    print(l_resp_json)
+    if l_resp_json['success']:
+        l_id = l_resp_json['id']
+        return redirect(f"/deployment/{l_id}")
+    else:
+        return f"Error {l_resp_json['error']}"    
 
-@frontend.route("/deployment/<id>", methods=['DELETE'])
+@frontend.route("/deployment/delete/<id>", methods=['POST'])
 def delete_deployment():
     pass
 
+@frontend.route("/deployment/list", methods=['GET'])
+def list_deployments():
+    l_api_reponse = requests.get(DEPLOYMENT_URL_LIST)
+    l_resp_json = l_api_reponse.json()
+    # l_obj = json.loads(l_resp_json)
+    return render_template('deployment_list.j2', obj=l_resp_json)
 
 if __name__ == "__main__":
     frontend.run(
